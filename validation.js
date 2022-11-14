1a1bec302881a37f069e63efa278a337899e186f
Pat Escalona <pat.escalona7@gmail.com>
2022-11-13 20:00:54 -0600

    finished as I want to be with this topic

--- validation.js
+++ validation.js
@@ -1,16 +1,62 @@
 // validation.js
 
+addSubmitButtonListener = () => {
+    const btn = document.querySelector('.submit');
+    btn.addEventListener('click', (e) => {
+
+    }
+}
+
 addValidationListeners = () => {
-    const email = document.querySelector('email-id');
+    console.log('in addValidationListeners');
+    emailValidation();
+    zipcodeValidation();
+};
+
+emailValidation = () => {
+    console.log('in emailValidation');
+    const email = document.querySelector('#email-id');
     email.addEventListener('input', (e) => {
+        if (email.validity.typeMismatch) {
+            email.setCustomValidity('I am expecting and e-mail address!');
+            email.reportValidity();
+        } else {
+            email.setCustomValidity('');
+        }
     })
 }
+
+zipcodeValidation = () => {
+    console.log('in zipcodeValidation');
+    const zipcode = document.querySelector('#zip-code');
+    const zipcodeRegexp = /^[0-9]{5}(?:-[0-9]{4})?$/;
+    zipcode.addEventListener('input', (e) => {
+        const isValid = zipcodeRegexp.test(e.target.value);
+        resultZipCode = isValid !== true ? e.preventDefault() : e.target.value;
+        console.log(resultZipCode);
+    })
+
+    // zipcodeRegexp = /^[0-9]{5}(?:-[0-9]{4})?$/;
+
+};
 
 
 
 
 window.addEventListener('DOMContentLoaded', (e) => {
     console.log('DOM loaded and parsed');
-
     addValidationListeners();
+    addSubmitButtonListener();
 })
+
+
+// email.addEventListener("input", () => {
+//     const isValid = email.value.length === 0 || emailRegExp.test(email.value);
+//     if (isValid) {
+//       email.className = "valid";
+//       error.textContent = "";
+//       error.className = "error";
+//     } else {
+//       email.className = "invalid";
+//     }
+//   });
